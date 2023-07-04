d782c8eb2318fd3e8850454e9bcbc7b682ca6f14
Kleo Kocaqi <kleo@digitsapiens.com>
2023-07-04 09:07:53 +0200

    Recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+
+  recipes: Recipe[] = [
+    new Recipe("Recipe1", "Desc1", "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"),
+    new Recipe("Recipe2", "Desc2", "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg")
+  ];
+
+  ngOnInit(): void {
+  }
 
 }
